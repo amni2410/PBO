50a869d25395e2f1db7f762668bf9ea1c087d370
amni2410 <45454569+amni2410@users.noreply.github.com>
2020-11-03 22:14:53 +0700

    Add files via upload

---
+++ src/com/company/TKT2.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class TKT2 {
+    public static void main (String [] args) {
+        int a, b, c, d, e;
+        int hasil;
+        a=10;
+        b=20;
+        c=45;
+        d= a + b + c;
+        e= 3;
+        hasil= d /=e;
+        System.out.println("angka 1 =" + a);
+        System.out.println("angka 2 =" + b);
+        System.out.println("angka 3 =" + c);
+        System.out.println("rata-ratanya adalah" + hasil);
+
+    }
+}
